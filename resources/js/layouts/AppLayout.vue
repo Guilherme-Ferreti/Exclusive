6111e699762835c8d3f89a4304bf00ac862b47a8
Guilherme Ferreti <guiferretis@gmail.com>
2025-12-28 14:59:11 -0300

    fix: stick footer to bottom of page

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,13 +1,15 @@
 <template>
   <AppOverlay />
-  <header class="app-container | h-4.5 border-b border-gray-300 py-1">
-    <TheMobileNavbar />
-    <TheDesktopNavbar />
-  </header>
-  <main class="app-container my-3.75 min-h-[calc(100vh-(--spacing(4.5)))]">
-    <slot />
-  </main>
-  <TheFooter />
+  <div class="flex h-screen flex-col">
+    <header class="app-container | h-4.5 border-b border-gray-300 py-1">
+      <TheMobileNavbar />
+      <TheDesktopNavbar />
+    </header>
+    <main class="app-container my-3.75">
+      <slot />
+    </main>
+    <TheFooter class="mt-auto" />
+  </div>
 </template>
 
 <script setup lang="ts">
