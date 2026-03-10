6249cd68a3dd2a5d0a7e510c4d83344c4753c7cc
Guilherme Ferreti <guiferretis@gmail.com>
2026-03-10 20:46:58 -0300

    fix: use container queries to improve ProductPreviewGrid

--- resources/js/components/ProductPreviewGrid.vue
+++ resources/js/components/ProductPreviewGrid.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex flex-wrap gap-1.5">
-    <ProductPreviewCard
-      v-for="product in products"
-      :key="product.id"
-      :product="product"
-    />
+  <div class="@container/product-preview-grid">
+    <div class="product-preview-grid">
+      <ProductPreviewCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
   </div>
 </template>
 
@@ -15,3 +17,30 @@ defineProps<{
   products: App.Data.Inertia.ProductPreview[];
 }>();
 </script>
+
+<style scoped>
+@reference 'tailwindcss';
+
+.product-preview-grid {
+  --card-width: calc(var(--spacing) * 17);
+  --gap: calc(var(--spacing) * 1.5);
+
+  --two-cols-max-width: calc((var(--card-width) * 2) + var(--gap));
+  --three-cols-max-width: calc((var(--card-width) * 3) + (var(--gap) * 2));
+  --four-cols-max-width: calc((var(--card-width) * 4) + (var(--gap) * 3));
+
+  @apply mx-auto grid w-full max-w-(--card-width) place-items-center gap-(--gap);
+
+  @container (width >= 568px) {
+    @apply max-w-(--two-cols-max-width) grid-cols-2;
+  }
+
+  @container (width >= 864px) {
+    @apply max-w-(--three-cols-max-width) grid-cols-3;
+  }
+
+  @container (width >= 1160px) {
+    @apply max-w-(--four-cols-max-width) grid-cols-4;
+  }
+}
+</style>
